80f961784a79c6a759666fb4ebd76482aef6e3a4
tiuphun <phuong.nt210692@sis.hust.edu.vn>
2023-12-16 16:26:37 +0700

    fixed user input read incorrectly

---
+++ final/notes.md
@@ -0,0 +1,2 @@
+đang lỗi input bạn yêu nhé
+input size = 8x vẫn bị tính k hợp lệ
--- final/raid.asm
+++ final/raid.asm
@@ -16,6 +16,7 @@ Disk3:      .byte       0, 0, 0, 0
 
 input:      .space      256
 errMsg:     .asciiz     "Input string should have size as a multiple of 8. Please try again.\n"
+counter:	.word		1
 
 diskHeader: .asciiz     "Disk 1 \t\t Disk 2 \t\t Disk 3\n"
 border:     .asciiz     "---------------\t\t---------------\t\t---------------"
@@ -24,7 +25,7 @@ tab:        .asciiz     "\t\t"
 
 .text
 .globl main
-
+main:
 #---------------------------------------------------------------------------
 # Get user input
 # param[in]
@@ -39,6 +40,7 @@ readInput:  li      $v0, 8
             jal     strlen
             remu    $a1, $v0, 8
             bnez    $a1, invalidInput
+            j       disk
 invalidInput: li    $v0, 4
             la      $a0, errMsg
             syscall
@@ -50,7 +52,8 @@ strlenLoop: lb      $t2, 0($t0)
             addiu   $t0, $t0, 1         # goto next byte
             addiu   $t1, $t1, 1         # increment the counter
             j       strlenLoop
-strlenEnd:  move    $v0, $t1            # return length of str
+strlenEnd:  addiu   $t1, $t1, -1        # exclude the count for null terminator 
+            move    $v0, $t1            # return length of str
             jr		$ra				
 
 #---------------------------------------------------------------------------
@@ -58,11 +61,10 @@ strlenEnd:  move    $v0, $t1            # return length of str
 # param[in]
 # return
 #---------------------------------------------------------------------------
-    lw      $t0, counter
-
-    beq     $t0, 1, parityD3
-    beq     $t0, 2, parityD2
-    beq     $t0, 3, parityD1
+disk:       lw      $t0, counter
+            beq     $t0, 1, parityD3
+            beq     $t0, 2, parityD2
+            beq     $t0, 3, parityD1
 
 # Use Disk3 for parity and Disk1 and Disk2 for data
 parityD3:   la      $t1, Disk1          
@@ -87,10 +89,10 @@ nextTurn:   addiu   $t0, $t0, 1
             sw      $t0, counter
             li      $t1, 4                  # if counter > 3, reset
             bgt     $t0, $t1, resetTurn
-            j       main
+            j       printDisk
 resetTurn:  li      $t0, 1
             sw      $t0, counter
-            j       main
+            j       printDisk
 
 #---------------------------------------------------------------------------
 # Calculate parity
